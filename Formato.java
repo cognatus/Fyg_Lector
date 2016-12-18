ed716822b4cd890f39a6965e483d638575ec9696
cognatus <diegojmartinezzm@gmail.com>
2016-12-18 14:12:30 -0600

    Formato creado

---
+++ Formato.java
@@ -0,0 +1,53 @@
+public class Formato {
+
+	private String nombre;
+	private String rfc;
+	private String numEmpleado;
+	private String fecha;
+
+	public Formato(){
+		this.nombre = "";
+		this.rfc = "";
+		this.numEmpleado = "";
+		this.fecha = "";
+	}
+
+	public limpia(){
+		this.nombre = "";
+		this.rfc = "";
+		this.numEmpleado = "";
+		this.fecha = "";
+	}
+
+	public void setNombre (String nombre){
+		this.nombre = nombre;
+	}
+	
+	public String getNombre (){
+		return this.nombre;
+	}
+
+	public void setRfc (String rfc){
+		this.rfc = rfc;
+	}
+	
+	public String getRfc (){
+		return this.rfc;
+	}
+
+	public void setFecha (String fecha){
+		this.fecha = fecha;
+	}
+	
+	public String getFecha (){
+		return this.fecha;
+	}
+
+	public void setNumEmpleado (String numEmpleado){
+		this.numEmpleado = numEmpleado;
+	}
+	
+	public String getNumEmpleado (){
+		return this.numEmpleado;
+	}
+}
--- Lector.java
+++ Lector.java
@@ -6,36 +6,96 @@ import java.util.ArrayList;
 
 public class Lector {
 
+	List<String> lineas = Files.readAllLines(Paths.get("tabula-SimpleIndex_Prueba2.csv"));
+	Formato aux = new Formato();
+	ArrayList<String> documentosCSV = new ArrayList<String>();
+
 	public static void main(String[] args) {
 
 		try{
 
-			List<String> lineas = Files.readAllLines(Paths.get("tabula-SimpleIndex_Prueba2.csv"));
-			ArrayList<String> documentosCSV = new ArrayList<String>();
-			String aux = "";
+			
+
+
+			for (int i = lineas.size - 1; i >= 0; i--) {
+
+				linea = lineas.get(i);
+				
+				if ( linea.contains("numero de empleado") ) {//buscar numero de empleado
+
+					aux.setNumEmpleado(obtenNumEmpleado(linea));
+					
+				}else if ( linea.contains("C.") || linea.contains("C,") ) {//buscar nombre
+					
+					aux.setRfc(obtenNumEmpleado(linea));
+
+				} else{//buscar rfc
+
+
+
+				}
+
+				
+
+				
+
+				//buscar fecha
+				if (lineas.get(i).charAt(0) == '"' && lineas.get(i).charAt(1) == '"') {
+
+					documentosCSV.add(aux);
+					System.out.println(aux);
+					aux = "";
+				}else {
+					aux += lineas.get(i)+"\n";
+				}
+			}
 
 			for (int i=0; i < lineas.size(); i++) {
 
+				
+
 				//separa los distintos documentosCSV
-				if (lineas.get(i).charAt(0) == '"' && lineas.get(i).charAt(1) == '"' && i != 0) {
-					
+				if (lineas.get(i).charAt(0) == '"' && lineas.get(i).charAt(1) == '"') {
+
 					documentosCSV.add(aux);
+					System.out.println(aux);
 					aux = "";
+				}else {
+					aux += lineas.get(i)+"\n";
 				}
 
-				aux += lineas.get(i);
+				
 
 			}
 
-			for (String line : documentosCSV) {
-				System.out.println(line);
-			}
+			/*int aux2 = 0;
+			for (String documento : lineas) {
+
+				//System.out.println(documento);
+
+				//ArrayList<String> auxArray = ()documento.split("\n");
+
+				//System.out.println(documento);
+
+				for (String linea : documento.split("\n")) {
+					System.out.println(linea);
+					aux2++;
+				}
+
+				System.out.println(aux2);
+			}*/
 
 		}catch(Exception ex){
 
 			System.out.println("Error papu "+ex);
 
 		}
+	}
+
+	public String obtenNumEmpleado(String linea){
+
+		return "";
+
 	}
 
 }
